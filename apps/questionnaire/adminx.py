e4c8d85580ab8b6a326e53a48eafc54a88353610
zonglu <415390588@qq.com>
2018-03-27 01:18:44 +0800

    样式优化

--- apps/questionnaire/adminx.py
+++ apps/questionnaire/adminx.py
@@ -1,32 +1,44 @@
 # _*_ coding:utf-8 _*_
 import xadmin
-from .models import Questionnaire, Question, Choice, Answer
+from .models import Questionnaire, Question
 
 
-class QuestionInline:
+class QuestionInline(object):
     model = Question
     extra = 0
 
+
 class QuestionnaireAdmin(object):
-    list_display = ['name','show_questionnaire']
+    list_display = ['name', 'show_questionnaire']
     search_fields = ['name']
     list_filter = ['name']
+    # 列表页直接编辑
+    list_editable = ['name']
     model_icon = 'fas fa-clipboard-list'
     inlines = [QuestionInline]
+    # 根据更新时间倒序
+    # ordering = ['-update_time']
 
 
 class QuestionAdmin(object):
     list_display = ['text', 'type']
     search_fields = ['text']
     list_filter = ['type']
+    readonly_fields = ['sortnum']
     model_icon = 'fas fa-question'
+    # 不显示字段
+    # exclude = ['sortnum']
+    relfield_style = 'fk_ajax'
 
 
-class ChoiceAdmin(object):
-    list_display = ['text', 'tags', 'question']
-    search_fields = ['text', 'tags', 'question']
-    list_filter = ['text', 'tags', 'question']
-    model_icon = 'fas fa-list-ol'
+
+# class ChoiceAdmin(object):
+#     list_display = ['text', 'question', 'sortnum']
+#     search_fields = ['text', 'question']
+#     list_filter = ['text', 'question']
+#     model_icon = 'fas fa-list-ol'
+#     # 不显示字段
+#     exclude = ['sortnum']
 
 
 # class AnswerAdmin(object):
@@ -37,6 +49,5 @@ class ChoiceAdmin(object):
 
 xadmin.site.register(Questionnaire, QuestionnaireAdmin)
 xadmin.site.register(Question, QuestionAdmin)
-xadmin.site.register(Choice, ChoiceAdmin)
 # xadmin.site.register(Answer, AnswerAdmin)
 
--- apps/questionnaire/models.py
+++ apps/questionnaire/models.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 from users.models import UserProfile
+from django.utils.translation import ugettext as _
 
 CHOICES_TYPE = [('choice', u'单选'), ('multi', u'多选'), ('star', u'打星'), ('ask', u'问答')]
 
 
 class Questionnaire(models.Model):
-    name = models.CharField(max_length=128)
+    name = models.CharField(max_length=128, verbose_name=_(u"问卷标题"))
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
 
     def questions(self):
         return Question.objects.filter(questionnaire=self).order_by('sortnum')
@@ -15,7 +18,7 @@ class Questionnaire(models.Model):
         from django.utils.safestring import mark_safe
         return mark_safe("<a href='/questionnaire/take/%s' target='_blank'>预览问卷</a>" % self.id)
 
-    show_questionnaire.short_description = "预览"
+    show_questionnaire.short_description = u"预览"
 
     def __unicode__(self):
         return self.name
@@ -30,41 +33,27 @@ class Questionnaire(models.Model):
 
 
 class Question(models.Model):
-    questionnaire = models.ForeignKey(Questionnaire)
-    sortnum = models.IntegerField()
-    text = models.TextField(blank=True)
-    type = models.CharField(u"Type of question", max_length=32, choices=CHOICES_TYPE)
+    questionnaire = models.ForeignKey(Questionnaire, verbose_name=_(u"问卷"))
+    sortnum = models.IntegerField(default=1, verbose_name=_(u"序号"))
+    type = models.CharField(max_length=32, choices=CHOICES_TYPE, verbose_name=_(u"题型"))
+    text = models.CharField(max_length=128, verbose_name=_(u"问题"))
+    chice_text = models.TextField(blank=True, null=True, verbose_name=_(u"选项"), help_text=_(u"每行为一个选项"))
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name = '问题'
         verbose_name_plural = verbose_name
 
-    def chices(self):
-        return Choice.objects.filter(question=self).order_by('sortnum')
-
     def __unicode__(self):
-        return u'[%s] (%s) %s' % (self.questionnaire, self.sortnum, self.text)
-
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question)
-    sortnum = models.IntegerField()
-    text = models.TextField(u"Choice Text")
-    tags = models.CharField(u"Tags", max_length=64, blank=True)
-
-    class Meta:
-        verbose_name = '选项'
-        verbose_name_plural = verbose_name
-
-    def __unicode__(self):
-        return u'(%s) %d. %s' % (self.question.sortnum, self.sortnum, self.text)
+        return u'[%s] (%d) %s' % (self.questionnaire, self.sortnum, self.text)
 
 
 class RunInfo(models.Model):
     "Store the active/waiting questionnaire runs here"
     subject = models.ForeignKey(UserProfile)
-    questionnaire = models.ForeignKey(Questionnaire, blank=True, null=True)
-    created = models.DateTimeField(auto_now_add=True)
+    questionnaire = models.ForeignKey(Questionnaire)
+    create_time = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
         return "%s: %s, %s" % (self.runid, self.subject.surname, self.subject.givenname)
@@ -77,6 +66,8 @@ class Answer(models.Model):
     runinfo = models.ForeignKey(RunInfo)
     question = models.ForeignKey(Question)
     text = models.TextField(blank=True, null=True)
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
 
     def __unicode__(self):
         return "Answer(%s: %s, %s)" % (self.question.sortnum, self.subject.surname, self.subject.givenname)
