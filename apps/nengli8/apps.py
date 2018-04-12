f46c0bb21e9e09576f6f018082ce06d94c1cdb42
wahaha <m18037461990_1@163.com>
2018-04-12 12:25:31 +0800

    修改

---
+++ apps/nengli8/apps.py
@@ -0,0 +1,11 @@
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class Nengli8Config(AppConfig):
+    name = 'apps.nengli8'
+    verbose_name = u'nengli8后台'
+    # label = u'nengli8后台'
+
+
