d341af6e3947dcf7b348a679caa50662cd4e7fe9
loverPig <m18037461990_1@163.com>
2018-04-20 16:07:57 +0800

    company

---
+++ apps/company/apps.py
@@ -0,0 +1,10 @@
+# _*_ coding:utf-8 _*_
+
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class CompanyConfig(AppConfig):
+    name = 'company'
+    verbose_name = u'后台管理'
