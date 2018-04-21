cbb2dfb2ebc96823934536893311b10a5efcdc62
zonglu <415390588@qq.com>
2018-04-21 09:03:38 +0800

    删除无用代码

--- apps/companys/apps.py
+++
@@ -1,9 +0,0 @@
-# _*_ coding:utf-8 _*_
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class CompanysConfig(AppConfig):
-    name = 'companys'
-    verbose_name = u'企业'
