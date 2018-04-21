b3435946e22ce42ffe8e7f25755b249f40fc3c7e
loverPig <m18037461990_1@163.com>
2018-04-21 17:26:21 +0800

    companys to users
    企业  用户  多对多

--- apps/companys/adminx.py
+++ apps/companys/adminx.py
@@ -1,7 +1,11 @@
 # _*_ coding:utf-8 _*_
 
 import xadmin
-from .models import Companys
+from .models import Companys,Users
+
+class UsersChoice(object):
+    model = Users
+    extra = 0
 
 #企业
 class CompanysAdmin(object):
@@ -11,12 +15,19 @@ class CompanysAdmin(object):
     # 列表页直接编辑
     list_editable = ['name']
     model_icon = 'fas fa-clipboard-list'
-
+    inlines = [UsersChoice]
+xadmin.site.register(Companys, CompanysAdmin)
 # class DemoAdmin(object):
 #     list_display = ['name']
 #     search_fields = ['name']
 #     model_icon = 'fas fa-clipboard-list'
-
-
-
-xadmin.site.register(Companys, CompanysAdmin)
+#users
+# class UsersAdmin(object):
+#     list_display = ['name', 'username', 'password', 'tel', 'department', 'position', 'email', 'total_class']
+#     search_fields = ['name']
+#     # list_filter = ['name']
+#     # 列表页直接编辑
+#     list_editable = ['name']
+#     model_icon = 'fas fa-clipboard-list'
+#
+# xadmin.site.register(Users, UsersAdmin)
--- apps/companys/models.py
+++ apps/companys/models.py
@@ -28,3 +28,44 @@ class Companys(models.Model):
 
     def __unicode__(self):
         return self.name
+
+
+#用户基本信息表
+class Users(models.Model):
+    username = models.CharField(max_length=45, verbose_name=_(u"用户账号"))
+    name = models.CharField(max_length=45, verbose_name=_(u"姓名"))
+    # photo = models.CharField(max_length=1000, verbose_name=_(u"头像"))
+    password = models.CharField(max_length=45, verbose_name=_(u"密码"))
+    # companyID = models.IntegerField(default=0,  verbose_name=_(u"公司ID"))
+    department = models.CharField(max_length=45, verbose_name=_(u"部门"))
+    position = models.CharField(max_length=45, verbose_name=_(u"职位"))
+    # openid = models.CharField(max_length=45, verbose_name=_(u"微信openid"))
+    # qq = models.CharField(max_length=45, verbose_name=_(u"QQ"))
+    tel = models.CharField(max_length=45, verbose_name=_(u"电话"))
+    email = models.CharField(max_length=45, verbose_name=_(u"邮箱"))
+    # notice_wenda = models.BooleanField(max_length=45, verbose_name=_(u"问答通知"))
+    # notice_pinglun = models.BooleanField(max_length=45, verbose_name=_(u"评论通知"))
+    # notice_sendmail = models.BooleanField(max_length=45, verbose_name=_(u"问答评论发送邮箱"))
+    # total_hours = models.IntegerField(default=0,  verbose_name=_(u"累计学时"))
+    total_class = models.IntegerField(default=0,  verbose_name=_(u"学习课程"))
+    # total_day = models.IntegerField(default=0,  verbose_name=_(u"累计天数"))
+    # classID = models.IntegerField(default=0,  verbose_name=_(u"班级id"))
+    # dayBefor = models.DateTimeField( verbose_name=_(u"上次时间"))
+    # dayFirst = models.DateTimeField( verbose_name=_(u"连续登陆，第一次登陆"))
+    # total_score = models.FloatField(max_length=12, verbose_name=_(u"总成绩"))
+    # class_finish = models.IntegerField(default=0,  verbose_name=_(u"已完成课程"))
+    # state = models.BooleanField(max_length=1, verbose_name=_(u"是否有效"))
+    # new_ans = models.BooleanField(max_length=1, verbose_name=_(u"是否有新的回复"))
+    # classStudent = models.CharField(max_length=45, verbose_name=_(u"班级"))
+    companyid = models.ForeignKey(Companys, default=0, to_field='id',verbose_name=_(u"公司"))
+    # language = models.CharField(max_length=45, verbose_name=_(u"语言（1中文，2英文）"))
+    class Meta:
+        verbose_name = '用户'
+        verbose_name_plural = verbose_name
+
+    def __unicode__(self):
+        return self.name
+
+class ComtoUsers(models.Model):
+    companys = models.ForeignKey(Companys, to_field='id',verbose_name= _(u"公司"))
+    users = models.ForeignKey(Users,  to_field='id', verbose_name=_(u"用户"))
