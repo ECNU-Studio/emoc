7bb8f969ff6903e3f3e6c66c9d79c0977d1dd001
loverPig <m18037461990_1@163.com>
2018-04-29 10:21:10 +0800

    课程管理

--- apps/useradmin/urls.py
+++ apps/useradmin/urls.py
@@ -5,6 +5,7 @@ from .views import *
 
 urlpatterns = [
     # 后台管理首页
-    url(r'manage/courses$', manage_courses),
+    # url(r'manage/courses$', manage_courses),
+    url(r'manage/(?P<courses_id>[0-9]+)/$',manage_courses.as_view(), name='courses'),
 
 ]
--- apps/useradmin/views.py
+++ apps/useradmin/views.py
@@ -1,9 +1,18 @@
 # _*_ coding:utf-8 _*_
-from django.views.generic import View
+import json
 from django.shortcuts import render
+from django.views.generic import View
 from django.http import HttpResponse
 from django.shortcuts import render
+from courses.models import *
 
-
-def manage_courses(request):
-    return render(request, 'admin_courses.html')
+class manage_courses(View):
+    def get(self, request, courses_id=None, preview=1):
+        all_courses = courses.objects.all()
+        org_nums = courses.count()
+        # 反解析URL
+        return render(request, 'templates/admin_courses.html', {
+             'org_nums': org_nums,
+            'all_courses': all_courses,
+             'preview': preview
+         })
